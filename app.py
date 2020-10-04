795b5e93f4ff2fa7b02dca4fbeb947a7f33434ce
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-10-04 16:28:03 +0200

    Card

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request, redirect
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/mypage/me")
+def me():
+    return render_template("first_page.html")
+
+
+@app.route("/mypage/contact")
+def contact():
+    return render_template("second_page.html")
+
+
+@app.route('/mypage/contact', methods=['POST'])
+def contact_post():
+    if request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect("/")
+
+
